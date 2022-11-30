04c0a903b9a0a2d876c19cf5182e3936080d189e
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2022-11-30 10:16:42 +0800

    做到一半

--- routers/vote.js
+++ routers/vote.js
@@ -7,9 +7,18 @@ const pool = require("../utils/db");
 // 取得票所
 // http://localhost:3001/api/vote/vote_place
 router.get("/vote_place", async (req, res) => {
-  let [result] = await pool.execute(`SELECT * FROM vote_place `);
+  let [result] = await pool.execute(
+    `SELECT vote_place.*,sum(candidate.number_of_votes)vote FROM vote_place JOIN candidate ON vote_place.id=candidate.vote_place_id GROUP BY vote_place.id `
+  );
   res.json(result);
 });
+
+// router.get("/vote_place", async (req, res) => {
+//   let [result] = await pool.execute(
+//     `SELECT *  FROM vote_place  `
+//   );
+//   res.json(result);
+// });
 
 //取得候選人/票所
 // http://localhost:3001/api/vote/candidate
@@ -32,7 +41,6 @@ router.patch("/cnt/patch", async (req, res) => {
         `UPDATE candidate SET cnt=?,number_of_votes=? WHERE vote_place_id=? && id=? `,
         [data.cnt, data.number_of_votes, data.vote_place_id, data.id]
       );
-     
     }
     res.json("susses");
   } catch (err) {
@@ -43,11 +51,34 @@ router.patch("/cnt/patch", async (req, res) => {
 //查看排名
 // http://localhost:3001/api/vote/ranking
 router.get("/ranking", async (req, res) => {
-  let [result] = await pool.execute(`SELECT * FROM candidate `);
+  let [result] = await pool.execute(
+    `SELECT name,sum(number_of_votes)total,number,partisan FROM candidate  GROUP BY name , number ORDER BY total  DESC`
+  );
   res.json(result);
 });
 
+//取得該編號的選區資訊
+// http://localhost:3001/api/vote/ranking/number
+router.post("/ranking/number", async (req, res) => {
+  let [data] = await pool.execute(
+    `SELECT * FROM candidate WHERE number=? GROUP BY number=?`,
+    [req.body.number, req.body.number]
+  );
+  let [lConstituency] = await pool.execute(
+    `SELECT vote_place.*,candidate.number,sum(candidate.number_of_votes)a FROM vote_place JOIN candidate ON vote_place.id=candidate.vote_place_id WHERE number=? && constituency_id=1 GROUP BY constituency_id`,
+    [req.body.number]
+  );
+  let [lSub] = await pool.execute(
+    `SELECT vote_place.*,candidate.number,sum(candidate.number_of_votes)vote FROM vote_place JOIN candidate ON vote_place.id=candidate.vote_place_id WHERE number=? && constituency_id=1 GROUP BY sub`,
+    [req.body.number]
+  );
+  let [lNeighbor] = await pool.execute(
+    `SELECT vote_place.*,candidate.number,sum(candidate.number_of_votes)vote FROM vote_place JOIN candidate ON vote_place.id=candidate.vote_place_id WHERE number=? && constituency_id=1 GROUP BY neighbor`,
+    [req.body.number]
+  );
 
+  res.json({data,lConstituency,lSub,lNeighbor});
+});
 
 // 匯出
 module.exports = router;
